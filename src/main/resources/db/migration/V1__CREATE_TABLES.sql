a08ca855ae3ef152506a6727e2b7e38ba6b7e5f7
NurbolT <tolegenov.n.2705@gmail.com>
2022-05-26 13:38:22 +0600

    Add new tables
    -- create table "category"
    -- create table "product"

--- src/main/resources/db/migration/V1__CREATE_TABLES.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLES.sql
@@ -11,14 +11,32 @@ CREATE TABLE user_data(
 -- Таблица ролей
 CREATE TABLE role(
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    title VARCHAR(200) \NOT NULL,
+    title VARCHAR(200) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---
+-- Таблица связи юзеров и ролей
 CREATE TABLE user_role(
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    user_id BIGINT REFERENCES user_data(id),
-    role_id BIGINT REFERENCES role(id),
+    user_id BIGINT REFERENCES user_data(id) ON DELETE CASCADE,
+    role_id BIGINT REFERENCES role(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Таблица категории
+CREATE TABLE category(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- Таблица продукты
+CREATE TABLE product(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    price FLOAT NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    category_id BIGINT REFERENCES category(id) ON DELETE CASCADE
 );
